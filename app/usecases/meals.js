657497f4ef1ee7cc4150ee3373a18f6b53920676
stefan <stefan.goldschmidt@hpe.com>
2020-03-04 10:46:37 +0100

    init meals api

--- app/usecases/meals.js
+++ app/usecases/meals.js
@@ -1,5 +1,36 @@
-module.exports = function() {
-  this.onUpdate = (ctx)=>{
-  };
-  return this;
-};
+const gmaps = require("../services/gmaps");
+
+module.exports = function () {
+    this.onUpdate = (ctx) => {
+        if (ctx.update.message.text == "meals") {
+            ctx.reply("Directions");
+
+            gmaps.getDirections("Stuttgart DHBW Rotebühlplatz", "Gerber Stuttgart")
+                .then(data => {
+                    // console.log(data)
+                    /*  console.log(data.routes[0].legs[0])
+                      const distance = data.routes[0].legs[0].distance.text
+                      const duration = data.routes[0].legs[0].duration.text
+                      const steps = data.routes[0].legs[0].steps.map(step => {
+                          return step.html_instructions
+                      })
+                      console.log(steps)*/
+                    console.log("return from getDirections", data)
+                    ctx.reply(data);
+                });
+        }
+
+    }
+    return this;
+}
+
+/* else if (ctx.update.message.text == "places") {
+          /*  gplaces.getPlaceById(3).then((answer)=>{
+                console.log(`answer is ${answer}`);
+                ctx.reply(answer);
+            }).catch((err)=>{
+                ctx.reply("There has been an error, sorry");
+            });
+        }*/
+/*   };
+};*/
