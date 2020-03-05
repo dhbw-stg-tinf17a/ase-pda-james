2f66aa626e8abbe6ae14196a74dfa8e14e3bb721
stefan <stefan.goldschmidt@hpe.com>
2020-03-05 15:25:49 +0100

    Comment code

--- app/services/gmaps.js
+++ app/services/gmaps.js
@@ -1,11 +1,24 @@
 const axios = require("axios");
 const htmlToText = require("html-to-text");
 
+/*
+USAGE EXAMPLE
+ -----------------------------------------------------------------------
+// import gmaps
+const gmaps = require("<path-here>services/gmaps");
+
+gmaps.getDirections("Stuttgart DHBW Rotebühlplatz", "Gerber Stuttgart")
+    .then((data) => {      console.log(data);    });
+
+//logs a directions string
+ */
+
 const buildURL = (origin, destination, travelMode = "walking") => {
-  // encode Strings for URL
+  // encode input parameters for URL
   origin = encodeURIComponent(origin);
   destination = encodeURIComponent(destination);
 
+  // build URL
   const url = new URL("https://maps.googleapis.com/maps/api/directions/json?");
   const params = new URLSearchParams({
     origin: origin,
@@ -27,7 +40,10 @@ module.exports.getDirections = (origin, destination) => {
           // handle success
           const distance = response.data.routes[0].legs[0].distance.text;
           const duration = response.data.routes[0].legs[0].duration.text;
+
+          // reformat direction steps to single string
           const steps = response.data.routes[0].legs[0].steps.map((step) => {
+            // string comes as HTML => reformat to plain text string
             const htmlText = step.html_instructions;
             return htmlToText.fromString(htmlText);
           });
@@ -36,7 +52,7 @@ module.exports.getDirections = (origin, destination) => {
         })
         .catch(function(error) {
           // handle error
-          console.log(error);
+          console.error(error);
           reject(error);
         });
   });
