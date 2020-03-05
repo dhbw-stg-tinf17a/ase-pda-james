f37510ba2c8a06eaca238480176f6a0ac3f98cf6
Oliver Rudzinski <oliver@MacBook-Pro-Oliver-Rudzinski.local>
2020-03-05 13:45:46 +0100

    Add API call for getting stops based on input, implement in primitive use case

--- app/usecases/uniNotifier.js
+++ app/usecases/uniNotifier.js
@@ -1,5 +1,19 @@
+const vvs = require('../services/vvs')()
+
 module.exports = function() {
   this.onUpdate = (ctx)=>{
+    if (ctx.update.message.text == "vvs") {
+      vvs.getStopId("Hulb").then((res) => {
+        console.log("Stop Names and IDs:\n" + res)
+        ctx.reply("Which station do you mean?\n")
+        res.forEach((stop) => {
+          ctx.reply(stop.stopName + " (" + stop.stopId + ")")
+        })
+      }).catch((err) => {
+        console.log(err)
+        ctx.reply("An error occurred.")
+      }
+    )}
   };
   return this;
 };
