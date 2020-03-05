0a7e487fe143190fd3d048ba28eebfaa8000175c
Bengin <bengin@cetindere.de>
2020-03-05 09:18:24 +0100

    fix formatting and ==undefined

--- app/services/gplaces.js
+++ app/services/gplaces.js
@@ -20,29 +20,27 @@ module.exports = function() {
   this.getPlaceByText = (query, optionalParameters)=>{
     return new Promise((resolve, reject)=>{
       let url;
-      if(optionalParameters==undefined)
-      {
+      if (optionalParameters) {
         url="https://maps.googleapis.com/maps/api/place/textsearch/json?query="
-          + query + "&key=" + process.env.GOOGLE_PLACES_KEY
-      }
-      else{
+        + query + optionalParameters + "&key=" + process.env.GOOGLE_PLACES_KEY;
+      } else {
         url="https://maps.googleapis.com/maps/api/place/textsearch/json?query="
-        + query + optionalParameters + "&key=" + process.env.GOOGLE_PLACES_KEY
+        + query + "&key=" + process.env.GOOGLE_PLACES_KEY;
       }
       axios.get(url)
           .then(function(response) {
             if (response.status == 200) {
               resolve(response.data);
             } else {
-              console.log(response.data)
+              console.log(response.data);
               reject(new Error("API Error"));
             }
           })
           .catch(function(error) {
-            console.log(error)
+            console.log(error);
             reject(new Error("Axios Error"));
           });
     });
-  }
+  };
   return this;
 };
