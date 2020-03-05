3d39923444549b69b5abeedd0b394395721b9319
Oliver Rudzinski <oliver@MacBook-Pro-Oliver-Rudzinski.local>
2020-03-05 15:23:33 +0100

    Add API call for getting public transportation trips

--- app/services/vvs.js
+++ app/services/vvs.js
@@ -4,7 +4,8 @@ module.exports = () => {
   this.getStopId = (input) => {
     return new Promise((resolve, reject) => {
       const validPoints = [];
-      const apiUrl = 'http://efastatic.vvs.de/vvs/XML_STOPFINDER_REQUEST?outputFormat=JSON&locationServerActive=1&type_sf=any&name_sf=' + input;
+      const apiUrl = 'http://efastatic.vvs.de/vvs/XML_STOPFINDER_REQUEST?outputFormat=JSON&locationServerActive=1&type_sf=any&name_sf=' +
+      input;
 
       axios.get(apiUrl).then((res) => {
         if (res.status == 200) {
@@ -21,6 +22,33 @@ module.exports = () => {
           reject(new Error('API error'));
         }
       });
+    });
+  };
+
+  this.getTrips = (originId, destinationId, date, time, isDepTime) => {
+    const trips = [];
+
+    let timeType = dep;
+    if (!isDepTime) timeType = arr;
+
+    const apiUrl = 'http://efastatic.vvs.de/vvs/XML_TRIP_REQUEST2?outputFormat=JSON&itdTime=' +
+      time + '&locationServerActive=1&name_origin=' +
+      originId + '&type_origin=stopID&name_destination=' +
+      destinationId + '&type_destination=stopID&itdDate=' +
+      date + '&ptOptionsActive=1&itdTripDateTimeDepArr=' +
+      timeType;
+
+    axios.get(apiUrl).then((res) =>{
+      if (res.status == 200) {
+        console.log(JSON.stringify(res.data));
+        res.data.trips.forEach((trip) => {
+          trips.push(trip);
+        });
+        console.log('Number of trips found: ' + trips.length);
+        resolve(trips);
+      } else {
+        reject(new Error('API Error'));
+      }
     });
   };
 
