cffc4ead29ea8f48affbd204f3ccf93ee0dcf2dd
Stach <melanie.stach@hpe.com>
2020-03-05 12:51:29 +0100

    fix formatting

--- app/services/gplaces.js
+++ app/services/gplaces.js
@@ -3,8 +3,8 @@ const axios = require("axios");
 module.exports = function() {
   this.getPlaceById = (id)=>{
     return new Promise((resolve, reject)=>{
-      axios.get("https://maps.googleapis.com/maps/api/place/details/json?placeid="
-      + id + "&key=" + process.env.GOOGLE_PLACES_KEY)
+      axios.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=" +
+      id + "&key=" + process.env.GOOGLE_PLACES_KEY)
           .then(function(response) {
             if (response.status == 200) {
               resolve(response.data);
@@ -20,29 +20,27 @@ module.exports = function() {
   this.getPlaceByText = (query, optionalParameters)=>{
     return new Promise((resolve, reject)=>{
       let url;
-      if(optionalParameters==undefined)
-      {
-        url="https://maps.googleapis.com/maps/api/place/textsearch/json?query="
-          + query + "&key=" + process.env.GOOGLE_PLACES_KEY
-      }
-      else{
-        url="https://maps.googleapis.com/maps/api/place/textsearch/json?query="
-        + query + optionalParameters + "&key=" + process.env.GOOGLE_PLACES_KEY
+      if (optionalParameters==undefined) {
+        url="https://maps.googleapis.com/maps/api/place/textsearch/json?query=" +
+          query + "&key=" + process.env.GOOGLE_PLACES_KEY;
+      } else {
+        url="https://maps.googleapis.com/maps/api/place/textsearch/json?query=" +
+        query + optionalParameters + "&key=" + process.env.GOOGLE_PLACES_KEY;
       }
       axios.get(url)
           .then(function(response) {
             if (response.status == 200) {
               resolve(response.data);
             } else {
-              console.log(response.data)
+              console.log(response.data);
               reject(new Error("API Error"));
             }
           })
           .catch(function(error) {
-            console.log(error)
+            console.log(error);
             reject(new Error("Axios Error"));
           });
     });
-  }
+  };
   return this;
 };
--- app/services/mailer.js
+++ app/services/mailer.js
@@ -1,27 +1,28 @@
-var nodemailer = require('nodemailer'); 
+const nodemailer = require("nodemailer");
 module.exports = function() {
   this.sendMail = (recipient) =>{
     return new Promise((resolve, reject)=>{
-      let transporter = nodemailer.createTransport({
-        service: 'gmail',
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
         auth: {
           user: process.env.MAILER_USER,
-          pass: process.env.MAILER_PASSWORD
-        }
+          pass: process.env.MAILER_PASSWORD,
+        },
       });
-      
-      var mailOptions = {
+
+      const mailOptions = {
         from: process.env.MAILER_USER,
         to: recipient,
         subject: "Krankmeldung",
-        text: "Guten Tag, aufgrund von Krankheit kann ich heute leider nicht an Ihrer Vorlseung teilnehmen. Mit freundlichen Grüßen" 
+        text: "Guten Tag, aufgrund von Krankheit kann ich heute leider nicht an Ihrer Vorlseung teilnehmen." +
+        "Mit freundlichen Grüßen",
       };
-      
-      transporter.sendMail(mailOptions, function(error, info){
+
+      transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
           reject(error);
         } else {
-          resolve('Email sent: ' + info.response);
+          resolve("Email sent: " + info.response);
         }
       });
     });
