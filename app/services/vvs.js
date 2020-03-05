7aae6515c75da336bac3d254c8dd743e1574e9e6
Oliver Rudzinski <oliver@MacBook-Pro-Oliver-Rudzinski.local>
2020-03-05 15:57:08 +0100

    Add API call for getting public transportation trips

--- app/services/vvs.js
+++ app/services/vvs.js
@@ -25,5 +25,32 @@ module.exports = () => {
     });
   };
 
+  this.getTrips = (originId, destinationId, date, time, isDepTime) => {
+    const trips = [];
+
+    let timeType = dep;
+    if (!isDepTime) timeType = arr;
+
+    const apiUrl = "http://efastatic.vvs.de/vvs/XML_TRIP_REQUEST2?outputFormat=JSON&itdTime=" +
+      time + "&locationServerActive=1&name_origin=" +
+      originId + "&type_origin=stopID&name_destination=" +
+      destinationId + "&type_destination=stopID&itdDate=" +
+      date + "&ptOptionsActive=1&itdTripDateTimeDepArr=" +
+      timeType;
+
+    axios.get(apiUrl).then((res) =>{
+      if (res.status == 200) {
+        console.log(JSON.stringify(res.data));
+        res.data.trips.forEach((trip) => {
+          trips.push(trip);
+        });
+        console.log("Number of trips found: " + trips.length);
+        resolve(trips);
+      } else {
+        reject(new Error("API Error"));
+      }
+    });
+  };
+
   return this;
 };
