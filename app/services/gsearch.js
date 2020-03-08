9c8091450a4fa3e2e8dfc12621ebb3a2b916af77
stefan <stefan.goldschmidt@hpe.com>
2020-03-08 12:39:32 +0100

    Refactor Code

--- app/services/gsearch.js
+++ app/services/gsearch.js
@@ -1,9 +1,33 @@
-module.exports = function() {
-  this.getSearchResults = (queryString) =>{
-    return new Promise((resolve, reject)=>{
-      // implement API calls
-      resolve("not implemented");
+const axios = require("axios");
+require("dotenv").config();
+
+module.exports.getSearchResults = (query) => {
+  return new Promise((resolve, reject) => {
+    // build URL
+    const url = new URL("https://www.googleapis.com/customsearch/v1?");
+    const params = new URLSearchParams({
+      key: process.env.GOOGLE_API_KEY,
+      cx: process.env.GOOGLE_CUSTOM_SEARCH_ENGINE_ID,
+      q: query,
+      // prettyPrint: true
     });
-  };
-  return this;
+    axios.get(url + params)
+        .then((response) => {
+          // handle success
+          console.log(response.data.items.link);
+          /* for (let i = 0; i < response.items.length; i++) {
+            const item = response.items[i];
+            // in production code, item.htmlTitle should have the HTML entities escaped.
+            document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
+          }*/
+          resolve(response.data.items.slice(0, 3).map((item)=>{
+            return item.title+"\n"+ item.link;
+          }).join("\n\n"));
+        })
+        .catch(function(error) {
+          // handle error
+          console.error(error);
+          reject(error);
+        });
+  });
 };
