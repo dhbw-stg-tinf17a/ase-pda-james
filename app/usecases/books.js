550a7ed4a387c2c2aeb216e8dc8eb1a4c6c2d8db
Erik Littwin <erik.littwin@gmail.com>
2020-03-08 15:46:10 +0100

    basic gcal implementation

--- app/usecases/books.js
+++ app/usecases/books.js
@@ -1,4 +1,5 @@
 const library = require("../services/springer")();
+const cal = require("../services/gcalendar");
 
 module.exports = function() {
   this.onUpdate = (ctx) => {
@@ -12,6 +13,18 @@ module.exports = function() {
 
         ctx.reply("You searched for \"user experience\".").then(() => {
           ctx.replyWithHTML(message);
+        });
+      }).catch(() => {
+        ctx.reply("There has been an error, sorry");
+      });
+    } else if (ctx.update.message.text === "books events") {
+      cal.getNextEvent().then((res) => {
+        const eventsMessage =
+            res.map((event) => (`<b>${ event.title }</b> (${ event.start.date || event.start.dateTime } -` +
+                `${ event.end.date || event.end.dateTime })`),
+            ).join("\n\n");
+        ctx.reply("You're next 15 events are:\n").then(() => {
+          ctx.replyWithHTML(eventsMessage);
         });
       }).catch(() => {
         ctx.reply("There has been an error, sorry");
