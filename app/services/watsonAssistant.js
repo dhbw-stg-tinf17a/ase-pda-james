50940444dc9b6674ddd8ed572badcf57bca793e1
Stach <melanie.stach@hpe.com>
2020-03-10 17:17:52 +0100

    move assisstant ID to env document

--- app/services/watsonAssistant.js
+++ app/services/watsonAssistant.js
@@ -11,7 +11,7 @@ module.exports = function() {
   this.createSession = ()=>{
     return new Promise((resolve, reject)=>{
       assistant.createSession({
-        assistantId: "16509501-c2cd-4d5c-87f8-30e678c34968",
+        assistantId: process.env.WATSON_ASSISSTANT_ID,
       })
           .then((res) => {
             console.log(JSON.stringify(res, null, 2));
@@ -26,7 +26,7 @@ module.exports = function() {
   this.deleteSession = (sessionId)=>{
     return new Promise((resolve, reject)=>{
       assistant.deleteSession({
-        assistantId: "16509501-c2cd-4d5c-87f8-30e678c34968",
+        assistantId: process.env.WATSON_ASSISSTANT_KEY,
         sessionId: sessionId,
       })
           .then((res) => {
@@ -42,7 +42,7 @@ module.exports = function() {
   this.sendInput = (sessionId, userInput)=>{
     return new Promise((resolve, reject)=>{
       assistant.message({
-        assistantId: "16509501-c2cd-4d5c-87f8-30e678c34968",
+        assistantId: process.env.WATSON_ASSISSTANT_ID,
         sessionId: sessionId,
         input: {
           "message_type": "text",
