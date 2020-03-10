928e1f4b710d10197a2f8b6ae72220906696db45
Oliver Rudzinski <oliver@MacBook-Pro-Oliver-Rudzinski.local>
2020-03-10 15:44:44 +0100

    Rewrite stop querying logic, add error handling

---
+++ app/services/_test.js
@@ -0,0 +1,19 @@
+res = {
+    legs: [{
+        start: {
+            stop_name:
+            time:
+            date:
+        }
+        end: {
+            stop_name
+            time:
+            date:
+        }
+        mode: {
+            type:
+            code:
+            destination:
+        }
+    }]
+}
--- app/services/vvs.js
+++ app/services/vvs.js
@@ -1,45 +1,109 @@
 const axios = require("axios");
+const errorPrefix = "[VVS Service Error] ";
+
+function VvsApiError(message, httpCode) {
+  const error = new Error(errorPrefix + message);
+  error.httpCode = httpCode;
+  return error
+}
+
+function VvsMultiplePointsError(message, points) {
+  const error = new Error(errorPrefix + message);
+  error.points = points;
+  return error;
+}
+
+function VvsUnresolvableKeywordError(message, keyword) {
+  const error = new Error(errorPrefix + message);
+  error.keyword = keyword;
+  return error;
+}
 
 module.exports = () => {
-  this.getStopId = (input) => {
+  this.getStopByKeyword = (key) => {
     return new Promise((resolve, reject) => {
-      const validPoints = [];
-      const apiUrl = "http://efastatic.vvs.de/vvs/XML_STOPFINDER_REQUEST?outputFormat=JSON&" +
-      "locationServerActive=1&type_sf=any&name_sf=" + input;
+      const apiUrl = "http://efastatic.vvs.de/vvs/XML_STOPFINDER_REQUEST";
+      const apiParams = {
+        params: {
+          outputFormat: "JSON",
+          locationServerActive: "1",
+          type_sf: "any",
+          anyObjFilter_sf: "10",
+          name_sf: key,
+        },
+      };
 
-      axios.get(apiUrl).then((res) => {
-        if (res.status == 200) {
-          console.log(JSON.stringify(res.data));
-          res.data.stopFinder.points.forEach((point) => {
-            validPoints.push({
-              stopId: point.stateless,
-              stopName: point.name,
-            });
-          });
-          console.log(validPoints);
-          resolve(validPoints);
-        } else {
-          reject(new Error("API error"));
+      axios.get(apiUrl, apiParams).then((res) => {
+        const pointRes = res.data.stopFinder.points;
+        const stopRes = res.data.stopFinder.itdOdvAssignedStops;
+
+        // --- ERROR HANDLING ------------------------------------------------------------------------------------------
+
+        // API response error
+        if (res.status != 200) {
+          VvsApiError.prototype = Object.create(Error.prototype);
+          const err = new VvsApiError("The API did not perform successfully.", res.status);
+
+          reject(err);
         }
+
+        // Query keyword not resolvable
+        if (typeof res === "undefined") {
+          VvsUnresolvableKeywordError.prototype = Object.create(Error.prototype);
+          const err = new VvsUnresolvableKeywordError("The query is not valid. " +
+            "Please provide a valid query or try again.", key);
+
+          reject(err);
+        }
+
+        // Query keyword ambiguous
+        if (Array.isArray(pointRes)) {
+          const points = [];
+          pointRes.forEach((point) => {
+            points.push(point.name);
+          });
+
+          VvsMultiplePointsError.prototype = Object.create(Error.prototype);
+          const err = new VvsMultiplePointsError("The query returns multiple addresses. " +
+            "Please specify your city in your query.\n", points);
+
+          reject(err);
+        }
+
+        // -------------------------------------------------------------------------------------------------------------
+
+        // Sort resulting stops array by distance (ascending)
+        if (Array.isArray(stopRes)) {
+          stopRes.sort((a, b) => {
+            return a.distance - b.distance;
+          });
+
+          resolve(stopRes[0]);
+        }
+        resolve(stopRes);
       });
     });
   };
 
-  this.getTrips = (originId, destinationId, date, time, isDepTime) => {
+  this.getTrip = (tripParams) => {
     return new Promise((resolve, reject) => {
       const trips = [];
 
       let timeType = "dep";
-      if (!isDepTime) timeType = "arr";
+      if (!tripParams.isDepTime) timeType = "arr";
 
-      const apiUrl = "http://efastatic.vvs.de/vvs/XML_TRIP_REQUEST2?outputFormat=JSON&itdTime=" +
-        time + "&locationServerActive=1&name_origin=" +
-        originId + "&type_origin=stopID&name_destination=" +
-        destinationId + "&type_destination=stopID&itdDate=" +
-        date + "&ptOptionsActive=1&itdTripDateTimeDepArr=" +
-        timeType;
+      const apiUrl = "http://efastatic.vvs.de/vvs/XML_TRIP_REQUEST2";
 
-      axios.get(apiUrl).then((res) =>{
+      const apiParams = {
+        outputFormat: "JSON",
+        name_origin: tripParams.originId,
+        name_destination: tripParams.destinationId,
+        itdTime: tripParams.time,
+        itdDate: tripParams.date,
+        itdTripDateTimeDepArr: timeType,
+      };
+
+      axios.get(apiUrl, apiParams).then((res) =>{
         if (res.status == 200) {
           console.log(JSON.stringify(res.data));
           res.data.trips.forEach((trip) => {
