9102660a8e27fc48da01b9f19af052ed2471f6d8
Erik Littwin <erik.littwin@gmail.com>
2020-03-11 11:11:58 +0100

    Close #7: implemented three GCal endpoints (#67)
    * remove parentheses
    
    * Freebusy implementation (but static)
    
    * create event function
    
    * fixed minor things
    
    * pulled out CalId

--- app/usecases/books.js
+++ app/usecases/books.js
@@ -18,7 +18,7 @@ module.exports = function() {
         ctx.reply("There has been an error, sorry");
       });
     } else if (ctx.update.message.text === "books events") {
-      cal.getNextEvent().then((res) => {
+      cal.getNextEvents().then((res) => {
         const eventsMessage =
             res.map((event) => (`<b>${ event.title }</b> (${ event.start.date || event.start.dateTime } -` +
                 `${ event.end.date || event.end.dateTime })`),
@@ -28,6 +28,38 @@ module.exports = function() {
         });
       }).catch(() => {
         ctx.reply("There has been an error, sorry");
+      });
+    } else if (ctx.update.message.text === "books freebusy") {
+      cal.getFreeBusy("2020-03-10T00:00:00+01:00",
+          "2020-03-20T00:00:00+01:00",
+          process.env.CALENDAR_ID).then((calendars) => {
+        if (calendars) {
+          ctx.reply("busy found");
+        } else {
+          ctx.reply("no busy found");
+        }
+      }).catch((err) => {
+        ctx.reply("error occurred");
+      });
+    } else if (ctx.update.message.text === "books newevent") {
+      const testEvent = {
+        summary: "Test from James",
+        start: {
+          dateTime: "2020-03-15T10:00:00+01:00",
+        },
+        end: {
+          dateTime: "2020-03-15T10:00:00+01:00",
+        },
+      };
+
+      cal.createEvent(testEvent).then((createdEvent) => {
+        if (createdEvent !== {}) {
+          ctx.reply(`The event ${createdEvent.summary} was created successfully!`);
+        } else {
+          ctx.reply("Sorry, no event was created.");
+        }
+      }).catch((err) => {
+        ctx.reply("Sorry, an error occurred!");
       });
     }
   };
