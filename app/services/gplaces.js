0d1103dac74124fb7a2ee85000808e6e5191cdbb
Stach <melanie.stach@hpe.com>
2020-03-11 08:53:50 +0100

    change api request code style

--- app/services/gplaces.js
+++ app/services/gplaces.js
@@ -1,35 +1,37 @@
 const axios = require("axios");
+const gPlacesDetailEndpoint = "https://maps.googleapis.com/maps/api/place/details/json";
+const gPlacesSearchEndpoint = "https://maps.googleapis.com/maps/api/place/textsearch/json";
 
 module.exports = function() {
   this.getPlaceById = (id)=>{
     return new Promise((resolve, reject)=>{
-      axios.get("https://maps.googleapis.com/maps/api/place/details/json?place_id=" +
-      id + "&key=" + process.env.GOOGLE_PLACES_KEY)
-          .then(function(response) {
-            if (response.status == 200) {
-              resolve(response.data);
-            } else {
-              reject(new Error("API Error"));
-            }
-          })
-          .catch(function(error) {
-            reject(new Error("Axios Error"));
-          });
+      axios.get(gPlacesDetailEndpoint, {
+        params: {
+          place_id: id,
+          key: process.env.GOOGLE_PLACES_KEY,
+        },
+      }).then(function(response) {
+        if (response.status == 200) {
+          resolve(response.data);
+        } else {
+          reject(new Error("API Error"));
+        }
+      }).catch(function(error) {
+        reject(new Error("Axios Error"));
+      });
     });
   };
   // with optionalParameters the search for places can be filtered e.g. only opened places.
   // All possible parameters can be found in the wiki
   this.getPlaceByText = (query, optionalParameters)=>{
     return new Promise((resolve, reject)=>{
-      let url;
-      if (optionalParameters) {
-        url="https://maps.googleapis.com/maps/api/place/textsearch/json?query=" +
-        query + optionalParameters + "&key=" + process.env.GOOGLE_PLACES_KEY;
-      } else {
-        url="https://maps.googleapis.com/maps/api/place/textsearch/json?query=" +
-        query + "&key=" + process.env.GOOGLE_PLACES_KEY;
-      }
-      axios.get(url)
+      const requiredParameters= {
+        query: query,
+        key: process.env.GOOGLE_PLACES_KEY,
+      };
+      const params = Object.assign(requiredParameters, optionalParameters);
+
+      axios.get(gPlacesSearchEndpoint, {params})
           .then(function(response) {
             if (response.status == 200) {
               resolve(response.data);
@@ -37,8 +39,7 @@ module.exports = function() {
               console.log(response.data);
               reject(new Error("API Error"));
             }
-          })
-          .catch(function(error) {
+          }).catch(function(error) {
             console.log(error);
             reject(new Error("Axios Error"));
           });
