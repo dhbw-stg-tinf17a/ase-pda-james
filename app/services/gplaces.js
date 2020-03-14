1a0da52dc66807e7f3a2c9307bbb54439fd22e98
Stach <melanie.stach@hpe.com>
2020-03-14 19:13:16 +0100

    add endpoint get formatted Addresses

--- app/services/gplaces.js
+++ app/services/gplaces.js
@@ -45,5 +45,51 @@ module.exports = function() {
           });
     });
   };
+  this.getFormattedAddress = (query, optionalParameters)=>{
+    return new Promise((resolve, reject)=>{
+      const requiredParameters= {
+        query: query,
+        key: process.env.GOOGLE_PLACES_KEY,
+      };
+      const params = Object.assign(requiredParameters, optionalParameters);
+
+      axios.get(gPlacesSearchEndpoint, {params})
+          .then(function(response) {
+            if (response.status == 200) {
+              let postalCode;
+              let street;
+              let addressArray;
+              const formatedAddress = [];
+              response.data.results.forEach((result) => {
+                postalCode = result.formatted_address.match(/\d{5}/);
+                addressArray = result.formatted_address.split(",");
+                if (addressArray.length === 3) {
+                  street=addressArray[0];
+                } else { // check for element with street name and house number
+                  addressArray.forEach((element) =>{
+                    if (element.test(/[A-Z][a-z]{?}\sd{?},/)) {
+                      street=element;
+                    }
+                  });
+                }
+                formatedAddress.push(
+                    {
+                      Straße: street,
+                      Postleitzahl: postalCode[0],
+                      Stadt: addressArray[1].replace(/\s\d{5}\s/ig, ""),
+                    },
+                );
+              });
+              resolve(formatedAddress);
+            } else {
+              console.log(response.data);
+              reject(new Error("API Error"));
+            }
+          }).catch(function(error) {
+            console.log(error);
+            reject(new Error("Axios Error"));
+          });
+    });
+  };
   return this;
 };
