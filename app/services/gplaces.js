adaae6bec8edf1323c805576216de73777203107
Stach <melanie.stach@hpe.com>
2020-03-15 19:09:20 +0100

    merge two functions into one

--- app/services/gplaces.js
+++ app/services/gplaces.js
@@ -24,52 +24,51 @@ module.exports = function() {
   };
   // with optionalParameters the search for places can be filtered e.g. only opened places.
   // All possible parameters can be found in the wiki
-  this.getPlacesByText = (query, optionalParameters)=>{
+  this.getPlacesByText = (query, type, location, radius, optionalParameters)=>{
     return new Promise((resolve, reject)=>{
-      const requiredParameters= {
-        query: query,
-        key: process.env.GOOGLE_PLACES_KEY,
-      };
-      const params = Object.assign(requiredParameters, optionalParameters);
+      if (location) {
+        const requiredParameters= {
+          location: location,
+          radius: radius,
+          rankby: "distance",
+          keyword: query,
+          type: type,
+          key: process.env.GOOGLE_PLACES_KEY,
+        };
+        const params = Object.assign(requiredParameters, optionalParameters);
 
-      axios.get(gPlacesTextSearchEndpoint, {params})
-          .then(function(response) {
-            if (response.status == 200) {
-              resolve(response.data);
-            } else {
-              console.log(response.data);
-              reject(new Error("API Error"));
-            }
-          }).catch(function(error) {
-            console.log(error);
-            reject(new Error("Axios Error"));
-          });
-    });
-  };
+        axios.get(gPlacesNearbySearchEndpoint, {params})
+            .then(function(response) {
+              if (response.status == 200) {
+                resolve(response.data);
+              } else {
+                console.log(response.data);
+                reject(new Error("API Error"));
+              }
+            }).catch(function(error) {
+              console.log(error);
+              reject(new Error("Axios Error"));
+            });
+      } else {
+        const requiredParameters= {
+          query: query,
+          key: process.env.GOOGLE_PLACES_KEY,
+        };
+        const params = Object.assign(requiredParameters, optionalParameters);
 
-  this.getPlacesNearby = (location, query, type, optionalParameters)=>{
-    return new Promise((resolve, reject)=>{
-      const requiredParameters= {
-        location: location,
-        rankby: "distance",
-        keyword: query,
-        type: type,
-        key: process.env.GOOGLE_PLACES_KEY,
-      };
-      const params = Object.assign(requiredParameters, optionalParameters);
-
-      axios.get(gPlacesNearbySearchEndpoint, {params})
-          .then(function(response) {
-            if (response.status == 200) {
-              resolve(response.data);
-            } else {
-              console.log(response.data);
-              reject(new Error("API Error"));
-            }
-          }).catch(function(error) {
-            console.log(error);
-            reject(new Error("Axios Error"));
-          });
+        axios.get(gPlacesTextSearchEndpoint, {params})
+            .then(function(response) {
+              if (response.status == 200) {
+                resolve(response.data);
+              } else {
+                console.log(response.data);
+                reject(new Error("API Error"));
+              }
+            }).catch(function(error) {
+              console.log(error);
+              reject(new Error("Axios Error"));
+            });
+      }
     });
   };
   return this;
