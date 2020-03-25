bddabd80e03ff49b681b2a43a9016b7977d7fd99
inf17017@lehre.dhbe-stuttgart.de <melanie.stach@hpe.com>
2020-03-25 12:08:33 +0100

    test skelleton

--- app/services/gplaces.test.js
+++ app/services/gplaces.test.js
@@ -7,13 +7,19 @@ jest.mock("axios");
 let gplaces;
 
 beforeEach(() => {
-  gplaces = require("./gplaces");
+  gplaces = require("./gplaces")();
 });
-describe("gplaces getPlaceByText", () => {
-  test("if correct data gets fetched", () => {
+describe("gplaces getPlaces", () => {
+  test("if data gets fetched if only query specified", () => {
     axios.get.mockResolvedValue({data: searchResponse});
-    gplaces.getPlaceByText("DHBW Stuttgart").then((data) => {
-      expect(data).toBeDefined();
-    });
+    return gplaces.getPlaces({query: "DHBW"}).then((data) => expect(data).toEqual(searchResponse));
+  });
+  test("if data gets fetched if only query specified", () => {
+    axios.get.mockResolvedValue({data: searchResponse});
+    return gplaces.getPlaces({location: "48.803790, 9.236430", radius: 200}).then((data) => expect(data).toEqual(searchResponse));
+  });
+  test("if data gets fetched if only query specified", () => {
+    axios.get.mockResolvedValue({data: searchResponse});
+    return gplaces.getPlaces().then((data) => expect(data).toEqual(searchResponse));
   });
 });
