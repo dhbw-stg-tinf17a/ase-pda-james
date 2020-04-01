450b109020838e411b8d95254de46297b2d23f7e
inf17017@lehre.dhbw-stuttgart.de <melanie.stach@hpe.com>
2020-04-01 08:17:42 +0200

    write gplaces tests + coverage report

--- app/services/gplaces.test.js
+++ app/services/gplaces.test.js
@@ -11,14 +11,49 @@ beforeEach(() => {
 describe("gplaces getPlaces", () => {
   test("if data gets fetched if only query specified", () => {
     axios.get.mockResolvedValue({data: searchResponse});
-    return gplaces.getPlaces({query: "DHBW"}).then((data) => expect(data).toEqual(searchResponse));
+    return gplaces.getPlaces({query: "DHBW"})
+        .then((data) => expect(data).toEqual(searchResponse))
+        .catch(() => fail());
   });
   test("if data gets fetched if only query specified", () => {
     axios.get.mockResolvedValue({data: searchResponse});
-    return gplaces.getPlaces({location: "48.803790, 9.236430", radius: 200}).then((data) => expect(data).toEqual(searchResponse));
+    return gplaces.getPlaces({location: "48.803790, 9.236430", radius: 200})
+        .then((data) => expect(data).toEqual(searchResponse))
+        .catch(() => fail());
   });
   test("if data gets fetched if only query specified", () => {
     axios.get.mockResolvedValue({data: searchResponse});
-    return gplaces.getPlaces().then((data) => expect(data).toEqual(searchResponse));
+    return gplaces.getPlaces({location: "48.803790, 9.236430"})
+        .then((data) => expect(data).toEqual(searchResponse))
+        .catch(() => fail());
+  });
+  test("if data gets fetched if only query specified", () => {
+    axios.get.mockResolvedValue({data: searchResponse});
+    return gplaces.getPlaces({query: "DHBW", location: "48.803790, 9.236430"})
+        .then((data) => expect(data).toEqual(searchResponse))
+        .catch(() => fail());
   });
 });
+
+describe("gplaces getFormattedAddress", () => {
+  test("if addresses get formatted", () => {
+    axios.get.mockResolvedValue({data: searchResponse});
+    return gplaces.getFormattedAddress({query: "DHBW"})
+        .then((data) => expect(data).toEqual([
+          {
+            street: "Im Wiesengrund 40",
+            postalCode: "70794",
+            city: "Filderstadt",
+            name: "Im Wiesengrund 40"},
+          {
+            street: "Im Wiesengrund 40",
+            postalCode: "70806",
+            city: "Kornwestheim",
+            name: "Im Wiesengrund 40"},
+        ]))
+        .catch(() => {
+          throw new Error("Fehlgeschlagen");
+        });
+  });
+});
+
