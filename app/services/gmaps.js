672eb0560c481e1b555cebab65498ecc0b61c478
stefan <stefan.goldschmidt@hpe.com>
2020-04-01 17:04:48 +0200

    Add optional arrival time parameter in getDirections

--- app/services/gmaps.js
+++ app/services/gmaps.js
@@ -2,42 +2,53 @@ const axios = require("axios");
 const htmlToText = require("html-to-text");
 require("dotenv").config({path: __dirname + "./../../.env"});
 /*
-USAGE EXAMPLE getDirections
+ USAGE EXAMPLE getDirections
  -----------------------------------------------------------------------
-// import gmaps
-const gmaps = require("<path-here>services/gmaps");
+ // import gmaps
+ const gmaps = require("<path-here>services/gmaps");
 
-gmaps.getDirections("Stuttgart DHBW Rotebühlplatz", "Gerber Stuttgart")
-    .then((data) => {      console.log(data);    });
+ gmaps.getDirections("Stuttgart DHBW Rotebühlplatz", "Gerber Stuttgart")
+ .then((data) => {      console.log(data);    });
 
-//logs a directions string
+ //logs a directions string
 
 
-USAGE EXAMPLE getGoogleMapsRedirectionURL
+ USAGE EXAMPLE getGoogleMapsRedirectionURL
  -----------------------------------------------------------------------
-const gmaps = require("<path-here>services/gmaps");
-gmaps.getGoogleMapsRedirectionURL("Gerber Stuttgart");
+ const gmaps = require("<path-here>services/gmaps");
+ gmaps.getGoogleMapsRedirectionURL("Gerber Stuttgart");
  */
 
-const buildURL = (origin, destination, travelMode) => {
+const buildURL = (origin, destination, travelMode, arrivalTime) => {
   // build URL
   const url = new URL("https://maps.googleapis.com/maps/api/directions/json?");
-  const params = new URLSearchParams({
+
+  let params = {
     origin: origin,
     destination: destination,
     mode: travelMode,
     language: "de-DE",
     key: process.env.GOOGLE_API_KEY,
-  });
+  };
+
+  if (arrivalTime !== null) {
+    params.arrival_time=arrivalTime;
+  }
+
+  params = new URLSearchParams(params);
 
   console.log("buildURL", url + params);
   return url + params;
 };
 
 // travelMode :   driving | walking | transit | bicycling
-module.exports.getDirections = (origin, destination, travelMode = "walking") => {
+// arrivalTime (integer) Specifies the desired time of arrival for transit directions,
+// in seconds since midnight, January 1, 1970 UTC
+
+// if you want to not set travelMode specifically but arrivalTime set it travelMode to undefined
+module.exports.getDirections = (origin, destination, travelMode = "walking", arrivalTime = null) => {
   return new Promise((resolve, reject) => {
-    axios.get(buildURL(origin, destination, travelMode))
+    axios.get(buildURL(origin, destination, travelMode, arrivalTime))
         .then((response) => {
           // handle success
           const distance = response.data.routes[0].legs[0].distance.text;
