5235c3775ef42438166897bd42fbe93980c6e6cb
Bengin <bengin@cetindere.de>
2020-04-06 14:39:29 +0200

    test

--- app/services/gmaps.js
+++ app/services/gmaps.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const htmlToText = require("html-to-text");
 require("dotenv").config({path: __dirname + "./../../.env"});
-const URL = require("url").URL;
 
 const buildURL = (config) => {
   // build URL
