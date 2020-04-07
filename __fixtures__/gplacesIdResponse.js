002a61b96af918301386e3770817f3865c8993fa
inf17017@lehre.dhbw-stuttgart.de <melanie.stach@hpe.com>
2020-04-07 10:43:42 +0200

    add endpoint + move beforeEach+ refactor test names

---
+++ __fixtures__/gplacesIdResponse.js
@@ -0,0 +1,302 @@
+/* eslint-disable */
+module.exports = {
+    "html_attributions":[
+ 
+    ],
+    "result":{
+       "address_components":[
+          {
+             "long_name":"48",
+             "short_name":"48",
+             "types":[
+                "street_number"
+             ]
+          },
+          {
+             "long_name":"Pirrama Road",
+             "short_name":"Pirrama Rd",
+             "types":[
+                "route"
+             ]
+          },
+          {
+             "long_name":"Sydney",
+             "short_name":"Sydney",
+             "types":[
+                "locality",
+                "political"
+             ]
+          },
+          {
+             "long_name":"Council of the City of Sydney",
+             "short_name":"Sydney",
+             "types":[
+                "administrative_area_level_2",
+                "political"
+             ]
+          },
+          {
+             "long_name":"New South Wales",
+             "short_name":"NSW",
+             "types":[
+                "administrative_area_level_1",
+                "political"
+             ]
+          },
+          {
+             "long_name":"Australia",
+             "short_name":"AU",
+             "types":[
+                "country",
+                "political"
+             ]
+          },
+          {
+             "long_name":"2009",
+             "short_name":"2009",
+             "types":[
+                "postal_code"
+             ]
+          }
+       ],
+       "adr_address":"<span class=\"street-address\">48 Pirrama Rd</span>, <span class=\"locality\">Sydney</span> <span class=\"region\">NSW</span> <span class=\"postal-code\">2009</span>, <span class=\"country-name\">Australia</span>",
+       "formatted_address":"48 Pirrama Rd, Sydney NSW 2009, Australia",
+       "formatted_phone_number":"(02) 8084 6690",
+       "geometry":{
+          "location":{
+             "lat":-33.8666114,
+             "lng":151.1958324
+          },
+          "viewport":{
+             "northeast":{
+                "lat":-33.86554936970849,
+                "lng":151.1970892302915
+             },
+             "southwest":{
+                "lat":-33.86824733029149,
+                "lng":151.1943912697085
+             }
+          }
+       },
+       "icon":"https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+       "id":"4f89212bf76dde31f092cfc14d7506555d85b5c7",
+       "international_phone_number":"+61 2 8084 6690",
+       "name":"Google Australia",
+       "opening_hours":{
+          "open_now":false,
+          "periods":[
+             {
+                "close":{
+                   "day":1,
+                   "time":"1730"
+                },
+                "open":{
+                   "day":1,
+                   "time":"0900"
+                }
+             },
+             {
+                "close":{
+                   "day":2,
+                   "time":"1730"
+                },
+                "open":{
+                   "day":2,
+                   "time":"0900"
+                }
+             },
+             {
+                "close":{
+                   "day":3,
+                   "time":"1730"
+                },
+                "open":{
+                   "day":3,
+                   "time":"0900"
+                }
+             },
+             {
+                "close":{
+                   "day":4,
+                   "time":"1730"
+                },
+                "open":{
+                   "day":4,
+                   "time":"0900"
+                }
+             },
+             {
+                "close":{
+                   "day":5,
+                   "time":"1700"
+                },
+                "open":{
+                   "day":5,
+                   "time":"0900"
+                }
+             }
+          ],
+          "weekday_text":[
+             "Monday: 9:00 AM – 5:30 PM",
+             "Tuesday: 9:00 AM – 5:30 PM",
+             "Wednesday: 9:00 AM – 5:30 PM",
+             "Thursday: 9:00 AM – 5:30 PM",
+             "Friday: 9:00 AM – 5:00 PM",
+             "Saturday: Closed",
+             "Sunday: Closed"
+          ]
+       },
+       "photos":[
+          {
+             "height":3024,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/105067373811767106297\">Shaun Craig</a>"
+             ],
+             "photo_reference":"CmRaAAAARiclfYKYkpcfHGAARsqF-ZY51dip9KqtESs7svg8xWAnhmTRZxjuTY-6U_hPylqg_6PZdJMnTTmouX_oSaSc4rkvgSLjURDoKQaV5TDakySWAienVueNRFPYFgwgvvogEhCq_tLp5xpOZbEEL6XyXUBGGhRgxRrmsK-k_VgcNH4m-egUl4ztOQ",
+             "width":4032
+          },
+          {
+             "height":3264,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/102493344958625549078\">Thomas Li</a>"
+             ],
+             "photo_reference":"CmRaAAAArWJVH9InnjGWX0ZiomiPSyiSoTCaKsSgOQjBBn15pbhgiPK1P28WAtVeo8eh2HVFnZpXTSLGFn7sIB8KsUPA_iLZmlLV4sjP-KNdisGCc88fK_ZHqi2jVDMYPmjJHNBIEhBa7_RSBR1P_RiVwnkVFNmXGhRXJe-jB6o6UDYUF5ErA70ONBXe6w",
+             "width":4912
+          },
+          {
+             "height":4016,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/107755640736541028674\">Jonah Deller</a>"
+             ],
+             "photo_reference":"CmRaAAAA8m9ibdE52_gmWsT76djTUcPMfZxNnNozpGLgFKRXMt2mBdjs2e8xBWz47FsAJrBweqbvlD9FPChf8KL5FkSFBSmmR29JHl2KkXaMrevaxEHiOy8dn4NTdAohQFUxLbK2EhAAEcGAzGtk7NNTrAmHfA6WGhR9ceaImIlkgV62VcVxwLo4R162kA",
+             "width":6016
+          },
+          {
+             "height":1365,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/105932078588305868215\">Maksym Kozlenko</a>"
+             ],
+             "photo_reference":"CmRaAAAA41cqvfwrrUTHtoKotch1-tcqop4FaqbwnG12vnJwWKV-zD4hZj62swMAxigvvk499b6y409sPq_Phc41iPpg9VjL0AiuRiqyoNlmcqYsQS3oqDS9GydxmOmvFbAkBqhqEhB-yEs8zZNSeZb8BJXkm6nkGhTP3fbyzPzh28lNa0kYS-Ij4I7q7w",
+             "width":2048
+          },
+          {
+             "height":3024,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/115886271727815775491\">Anthony Huynh</a>"
+             ],
+             "photo_reference":"CmRaAAAApmEY7IrIBVSF7tyn508Gv4WII5VRuXHi4MBoqjmnkAMlCviRHO4BPNjMB8HX2V5rCtuzG0n3hZvyCpvnJuSLynivINV83MeHfW3v-5CyPxVLnH5TEj-hMIcIn64xzMc3EhCPyTOvqak-i5BdSYF1vUC8GhRglRV8c8h9BoSAJYNo0a0Wfzu0jw",
+             "width":4032
+          },
+          {
+             "height":3024,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/100678816592586275978\">Jeremy Hsiao</a>"
+             ],
+             "photo_reference":"CmRaAAAAfSbyvIO8MMVsjdxy1hBVC6R2w7kPCJiuR_IhbPP53gUw6bj_R4-_yErrmJXoJaxchFosNmoM6wfBY_H6dcGn7TNpUyPZctx5wLY08mgWJ0XjKy6ma9txnRd65YU1THxgEhCOMKoSOTSGH39pKM5uQgOFGhRz23OZFiEiARp-7y70iZncqo41pw",
+             "width":4032
+          },
+          {
+             "height":4032,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/104578111747260232633\">Raymond LAM</a>"
+             ],
+             "photo_reference":"CmRaAAAA7wXZceV2lMai4AfLNPRA9eQtMPnTo30Lyx0u9pJz9lpWqRyDHAc7srLl4f9d_LW4AY541KU6LjgKyVPFHEDf9ZtSu4Qbhto2PQ4N9leXNIPXk8ltsp9Q4iXBu8_rMRKOEhAI9_Dr5iUYou7rUIQ98QlGGhRm_POnlsiTdtJTKdGrJOmT1FLugg",
+             "width":3024
+          },
+          {
+             "height":3024,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/102939237947063969663\">Jasen Baker</a>"
+             ],
+             "photo_reference":"CmRaAAAAWFNVv7xbnArvwn6zhU55GsLPFRIg38AB5HcuwlUO_FhN5CCwGsdKFGR6elumbguSRuRCuxGY5sq8aUzxXI4jlqi6k-YzdvQzsrZoMZLPO_0tchjXXuuDmRoxwf4dPGkOEhCZcQ3PliypJMXgO0vVlRSzGhQdTvX_ny6b8xvCgxJtbQ6Hw4sr_A",
+             "width":4032
+          },
+          {
+             "height":2160,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/100659772571554240861\">Paul Newson</a>"
+             ],
+             "photo_reference":"CmRaAAAAR9HCM3ELAuOz6j3lQ6V8kGXhxSDpJzOsIceAhxwhQDd_NDKyr8mxhHZmoZanK9ahrOgy6NOaTtvcOu6g3rKAe80DBo6eaenjMfV1qKlMa-3NWZSmAKruYiuWAn5svDsyEhChW69mIEXRVaDpU_stFMqrGhTZyPm-K6dS7oaY9LDXM0ND_zhHjQ",
+             "width":2160
+          },
+          {
+             "height":3024,
+             "html_attributions":[
+                "<a href=\"https://maps.google.com/maps/contrib/103695313945250148899\">Penny Christie</a>"
+             ],
+             "photo_reference":"CmRaAAAATPOuR9Tf4mcmGupWP-b6FMw6Q1KL0t0hEVRxSRUfuPkvlbWrU5Io9y9X-ERCCJvi54YP0Y-QYeF1n2pcuLUU12__EnOaFROP5oBdhMO52aWyOmyyGKOHgaAid87ZCEjpEhBWpjVGY4DhFphUTalnGihyGhQgbRNVeET_CTUJaFtzluNrZCtuvA",
+             "width":4032
+          }
+       ],
+       "place_id":"ChIJN1t_tDeuEmsRUsoyG83frY4",
+       "plus_code":{
+          "compound_code":"45MW+98 Pyrmont, New South Wales, Australia",
+          "global_code":"4RRH45MW+98"
+       },
+       "rating":4.1,
+       "reference":"ChIJN1t_tDeuEmsRUsoyG83frY4",
+       "reviews":[
+          {
+             "author_name":"LMN O",
+             "author_url":"https://www.google.com/maps/contrib/111096825911336321074/reviews",
+             "language":"en",
+             "profile_photo_url":"https://lh4.ggpht.com/-nI_b9eoPLsw/AAAAAAAAAAI/AAAAAAAAAAA/eS8NFjcIhB4/s128-c0x00000000-cc-rp-mo/photo.jpg",
+             "rating":5,
+             "relative_time_description":"a month ago",
+             "text":"The food here is fabulous.  Google runs great education courses.  We love Betcher!!",
+             "time":1579751199
+          },
+          {
+             "author_name":"LIMUEL SWANSON",
+             "author_url":"https://www.google.com/maps/contrib/100598724294317120607/reviews",
+             "language":"en",
+             "profile_photo_url":"https://lh3.ggpht.com/-VApg8x__oU8/AAAAAAAAAAI/AAAAAAAAAAA/fannhft5H2c/s128-c0x00000000-cc-rp-mo-ba3/photo.jpg",
+             "rating":5,
+             "relative_time_description":"3 months ago",
+             "text":"Really? I’m a Google map contributor so what else am I gonna do but to give this location five stars. \n\nYou know what they say, “don’t bite the hand that doesn’t feed you” Because yeah, we don’t get paid for this gig! \n\nAlso, Google likes me, something about algorithms.",
+             "time":1574851497
+          },
+          {
+             "author_name":"Guru s",
+             "author_url":"https://www.google.com/maps/contrib/106780570080138238260/reviews",
+             "language":"en",
+             "profile_photo_url":"https://lh3.ggpht.com/-DTR0hYa08Aw/AAAAAAAAAAI/AAAAAAAAAAA/xlkETZk8IJg/s128-c0x00000000-cc-rp-mo/photo.jpg",
+             "rating":5,
+             "relative_time_description":"5 months ago",
+             "text":"Its been more than 3 years since I got the PR with great guidance & professional work from Dr. Anna. I had approached him this week for getting the passport details update . No questions asked. And the update was done in a most professional way. \n\nIf you want to get your feedback on whether you are eligible for PR , stop anything else you are doing and Dr. Anna is the person you have to speak to.\n\nBe ready to get the honest feedback on your status.",
+             "time":1570001987
+          },
+          {
+             "author_name":"Simon Cookes",
+             "author_url":"https://www.google.com/maps/contrib/117324324636388331456/reviews",
+             "language":"en",
+             "profile_photo_url":"https://lh6.ggpht.com/-3H2w0qJn47s/AAAAAAAAAAI/AAAAAAAAAAA/qPdu7ic6TOc/s128-c0x00000000-cc-rp-mo/photo.jpg",
+             "rating":3,
+             "relative_time_description":"3 months ago",
+             "text":"Do not use Google Shared Drive (yet) - cannot move between content manager folders even though it says you can, cannot share folders with external clients or partners, if you did not create the file then you cant move it the author of the file needs to do that so takes days to move hundreds of files as you need to copy them individually. Stick with Dropbox (for now). Oh and unlimited space is not actually unlimited. Gmail a little clunky too. (I would have given 2 star but know some great folks working at Google.)",
+             "time":1574918492
+          },
+          {
+             "author_name":"Bastian M Goonewardena",
+             "author_url":"https://www.google.com/maps/contrib/116469171974372645286/reviews",
+             "language":"en",
+             "profile_photo_url":"https://lh5.ggpht.com/-zwvTCguP_4E/AAAAAAAAAAI/AAAAAAAAAAA/0pqwNUfNP-g/s128-c0x00000000-cc-rp-mo-ba5/photo.jpg",
+             "rating":5,
+             "relative_time_description":"5 months ago",
+             "text":"It was a honour for me to get in to google Australia office. The staff is extremely helpful very kind. I had high expectations even before entering the office but the architecture and the design of the office itself blew my expectations over the roof. \nOne thing I noticed is that google actually cares about their staff and their well-being.\n\nI was there at a google education conference. The food that they serve was very good, possibly the best ever food I have had in a conference.",
+             "time":1568879793
+          }
+       ],
+       "scope":"GOOGLE",
+       "types":[
+          "point_of_interest",
+          "establishment"
+       ],
+       "url":"https://maps.google.com/?cid=10281119596374313554",
+       "user_ratings_total":888,
+       "utc_offset":660,
+       "vicinity":"48 Pirrama Road, Sydney",
+       "website":"https://about.google/locations/?region=asia-pacific&office=sydney"
+    },
+    "status":"OK"
+ }
