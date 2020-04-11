2856862ccf7f5a4d09c30a1e13ce0a7080147d65
stefan <stefan.goldschmidt@hpe.com>
2020-04-11 14:57:11 +0200

    Add home address coordinates to preferences

--- app/usecases/start.js
+++ app/usecases/start.js
@@ -1,5 +1,7 @@
 const Markup = require("telegraf/markup");
 const preferences = require("../services/preferences")();
+const vvs = require("../services/vvs/vvs.js")();
+const gplaces = require("../services/gplaces")();
 
 module.exports = (db, oAuth2Client) => {
   const cal = require("../services/gcalendar")(db, oAuth2Client);
@@ -19,19 +21,41 @@ module.exports = (db, oAuth2Client) => {
         break;
 
       case "start_name":
-        ctx.reply("Hallo "+ waRes.context.name + "\n Sag mir deine Email");
+        preferences.set("name", waRes.context.name);
+        ctx.reply("Hallo " + waRes.context.name + "\n Sag mir deine Email");
         break;
 
       case "start_email":
-        ctx.reply(waRes.context.email +"\n Sag mir deine Adresse");
+        preferences.set("email", waRes.context.email);
+        ctx.reply(waRes.context.email + "\n Sag mir deine Adresse");
         break;
 
       case "start_address":
-        ctx.reply(waRes.context.address+"\n Sag mir deine Uni");
+        preferences.set("home_address", waRes.context.address);
+        vvs.getStopByKeyword(waRes.context.address).then((data) => {
+          if (Array.isArray(data)) {
+            const buttons = data.map((stop) => [Markup.callbackButton(stop.name, "start_sid_" + stop.stopID)]);
+            ctx.reply("Wähle deine Haltstelle zuhause aus:", Markup.inlineKeyboard(buttons).extra());
+          } else {
+            preferences.set("home_stop", data.stopID);
+            ctx.reply(`Ich habe deine Haltestelle: "${data.name}" gespeichert`);
+          }
+        });
+
+        gplaces.getPlaces({query: waRes.context.address}).then((data) => {
+          const location = data.results[0].geometry.location;
+          const coordinates = location.lat+", "+location.lng;
+          preferences.set("home_address_coordinates", coordinates);
+        }).catch((err) => {
+          console.log(err);
+        });
+
+        ctx.reply(waRes.context.address + "\n Sag mir deine Uni");
         break;
 
       case "start_uni":
-        ctx.reply(waRes.context.uni+"\n Sag mir deine Uni Email");
+        // preferences.set("home_address", waRes.context.address);
+        ctx.reply(waRes.context.uni + "\n Sag mir deine Uni Email");
         break;
 
       case "start_uni_email":
@@ -45,11 +69,11 @@ module.exports = (db, oAuth2Client) => {
         cal.authenticateUser(ctx);
         break;
 
-      case "start_is_authenticated": 
+      case "start_is_authenticated":
         ctx.reply("Authentifiziert");
 
         cal.getCalendars().then((cals) => {
-          const buttons = cals.map((resCal) => [Markup.callbackButton(resCal.summary, "start_cid_"+resCal.id)]);
+          const buttons = cals.map((resCal) => [Markup.callbackButton(resCal.summary, "start_cid_" + resCal.id)]);
           ctx.reply("Wähle deinen Vorlesungskalender aus:", Markup.inlineKeyboard(buttons).extra());
         }).catch((err) => {
           ctx.reply("Sorry, es gab einen Fehler!");
@@ -66,6 +90,11 @@ module.exports = (db, oAuth2Client) => {
     switch (indicator) {
       case "cid":
         preferences.set("lecture_cal_id", data);
+        ctx.reply("Danke! Das war's.");
+        break;
+      case "sid":
+        preferences.set("home_stop_id", data);
+        ctx.reply("Wie ist deine Uni Adresse?");
         break;
     }
   };
