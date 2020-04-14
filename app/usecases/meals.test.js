e00be314025edeee93c0e06e27bf8cc6c5985e82
stefan <stefan.goldschmidt@hpe.com>
2020-04-14 18:44:25 +0200

    Enhance Tests

--- app/usecases/meals.test.js
+++ app/usecases/meals.test.js
@@ -22,16 +22,16 @@ const mockReply = jest.fn((msg, param) => {
 const mockCallback = jest.fn((x) => {
 });
 
-const preferences = {set: mockSet, get: mockGet};
+let preferences;
 
 // jest.mock are hoisted so you can keep imports/require on top
-const spy = jest.fn(() => 42);
+const spyNumber = jest.fn(() => 42);
 
 jest.doMock("../services/gcalendar", () => {
-  return jest.fn((preferences)=> {
+  return jest.fn((preferences) => {
     this.getTimeUntilNextEvent = () => {
       return new Promise((resolve, reject) => {
-        resolve(spy());
+        resolve(spyNumber());
       });
     };
     return this;
@@ -43,6 +43,7 @@ describe("meals", () => {
     jest.resetModules();
     ctx = {reply: mockReply};
     meals = require("./meals")(preferences, null);
+    preferences = {set: mockSet, get: mockGet};
   });
 
   test("replyPlaces(...)", () => {
@@ -63,37 +64,67 @@ describe("meals", () => {
     expect(mockReply.mock.calls.length).toBe(1);
   });
 
+  test("replyMealsStart(...) preferences error", async () => {
+    try {
+      const promise = new Promise(((resolve, reject) => {
+        reject(new Error());
+      }));
+      await meals._replyMealsStart(promise, ctx, preferences);
+    } catch (e) {
+      expect(e).toBeDefined();
+    }
+  });
+
+  test("replyMealsStart(...) preferences not set", async () => {
+    const promise = new Promise(((resolve, reject) => {
+      resolve("17");
+    }));
+
+    const replyMealsStartGet = jest.fn((key, data) => {
+      return new Promise((resolve, reject) => {
+        resolve(null);
+      });
+    });
+
+    await meals._replyMealsStart(promise, ctx, {set: mockSet, get: replyMealsStartGet});
+    expect(mockReply).toHaveBeenCalled();
+  });
+
   test("onUpdate(...) default", () => {
     const waRes = {generic: [{text: "abc"}]};
     meals.onUpdate(ctx, waRes);
     expect(mockReply).toHaveBeenCalledWith("Ups, da ist etwas schiefgelaufen...");
   });
 
-  test("onUpdate(...) meals_start", () => {
-    const waRes = {generic: [{text: "meals_start"}]};
-    meals._replyMealsStart = mockCallback;
-    meals.onUpdate(ctx, waRes);
-    expect(mockCallback.mock.calls.length).toBe(1);
-  });
+  /* test("onUpdate(...) meals_start", () => {
+     const waRes = {generic: [{text: "meals_start"}]};
+     meals._replyMealsStart = ()=>new Error();
+     meals.onUpdate(ctx, waRes);
+     expect(mockReply).toHaveBeenCalledWith("Sorry, jetzt ist etwas schiefgelaufen!");
+     expect(mockReply).toContain("Sorry")
+     });*/
 
-  test("onUpdate(...) meals_start_with_food", () => {
+
+  test("onUpdate(...) meals_start_with_food catch", () => {
     const waRes = {generic: [{text: "meals_start_with_food"}]};
-    const mockCallback = jest.fn((x) => {
-    });
-    ctx = {reply: mockCallback};
-
     try {
       meals.onUpdate(ctx, waRes);
     } catch (e) {
       expect(e).toBeDefined;
     }
   });
+
 
   test("onUpdate(...) meals_food_only", () => {
-    const waRes = {generic: [{text: "meals_food_only"}]};
-    const mockCallback = jest.fn((x) => {
-    });
-    ctx = {reply: mockCallback};
+    const waRes = {generic: [{text: "meals_food_only"}], entities: [{value: "Test"}]};
+    meals._replyPlaces = mockCallback;
+    meals.onUpdate(ctx, waRes);
+    expect(mockCallback).toHaveBeenCalled();
+    expect(mockCallback).toHaveBeenCalledWith(ctx, "Test", preferences);
+  });
+
+  test("onUpdate(...) meals_food_only catch", () => {
+    const waRes = {generic: [{text: "meals_food_only"}], entities: null};
 
     try {
       meals.onUpdate(ctx, waRes);
@@ -104,9 +135,6 @@ describe("meals", () => {
 
   test("onUpdate(...) meals_start", () => {
     const waRes = {generic: [{text: "meals_start"}]};
-    const mockCallback = jest.fn((x) => {
-    });
-    ctx = {reply: mockCallback};
 
     try {
       meals.onUpdate(ctx, waRes);
@@ -115,14 +143,11 @@ describe("meals", () => {
     }
   });
 
-  test("onUpdate(...) meals_cron", () => {
+  test("onUpdate(...) meals_cron", async () => {
     const waRes = {generic: [{text: "meals_cron"}]};
-    const mockCallback = jest.fn((x) => {
-    });
-    ctx = {reply: mockCallback};
 
     try {
-      meals.onUpdate(ctx, waRes);
+      await meals.onUpdate(ctx, waRes);
     } catch (e) {
       expect(e).toBeDefined;
     }
