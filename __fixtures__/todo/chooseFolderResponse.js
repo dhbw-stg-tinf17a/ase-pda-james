1d199dd3b3ac86233e7c8a6f6e47aaf767df40f1
Bengin <bengin@cetindere.de>
2020-04-14 20:30:12 +0200

    >50%

---
+++ __fixtures__/todo/chooseFolderResponse.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    Name: "folder 1",
+  },
+  {
+    Name: "folder 2",
+  },
+];
--- app/services/todo.test.js
+++ app/services/todo.test.js
@@ -1,30 +1,30 @@
 const axios = require("axios");
 const getTodosResponse = require("../../__fixtures__/todo/getTodosResponse");
-
-const get = jest.fn((key)=>new Promise((resolve, reject)=>{
-  resolve("test_value");
-}));
-
-const set = jest.fn((key, value)=> new Promise((resolve, reject)=>{
-  resolve();
-}));
-
-const preferences = {
-  get,
-  set,
-};
+const chooseFolderResponse = require("../../__fixtures__/todo/chooseFolderResponse");
 
 jest.mock("axios");
-axios.get.mockResolvedValue({data: {
-  value: getTodosResponse,
-}});
 
 describe("ToDo", () => {
   let todo;
+  let preferences;
   beforeAll(()=>{
+  });
+  beforeEach(()=>{
+    preferences = {
+      get: jest.fn((key)=>new Promise((resolve, reject)=>{
+        resolve("test_value");
+      })),
+      set: jest.fn((key, value)=> new Promise((resolve, reject)=>{
+        resolve();
+      })),
+    };
     todo = require("./todo")(preferences);
   });
   test("getTodos returns 3 elements", () => {
+    axios.get.mockResolvedValue({data: {
+      value: getTodosResponse,
+    }});
+
     return todo.getTodos().then((todos)=>{
       expect(todos.length).toBe(3);
     }).catch((err)=>{
@@ -32,11 +32,46 @@ describe("ToDo", () => {
       fail("promise rejection");
     });
   });
-  // TODO: commiten und dann sortierung testen
-  // test("set 'example' triggers database updateOne function", ()=>{
-  //   prefs.set("example", "1337").then(()=>{
-  //     // console.log(db.collection("preferences").count.mock);
-  //     expect(updateOne).toHaveBeenCalled();
-  //   }).catch(fail);
-  // });
+  test("chooseFolder sends n buttons when user has n ToDo folders", ()=>{
+    const ctx = {
+      telegram: {
+        sendMessage: jest.fn(),
+      },
+    };
+    const chatId = "123";
+    const folderCount = chooseFolderResponse.length;
+
+    axios.get.mockResolvedValue({data: {
+      value: chooseFolderResponse,
+    }});
+
+    return todo.chooseFolder(ctx, chatId).then(()=>{
+      // check if number of buttons is the same as folders exist in ToDo
+      expect(ctx.telegram.sendMessage.mock.calls[0][2]
+          .reply_markup.inline_keyboard[0].length).toBe(folderCount);
+    });
+  });
+
+  test("authorizeUser sends link with oAuth2 Elements", ()=>{
+    const ctx = {
+      chat: {
+        id: "123",
+      },
+      reply: jest.fn(),
+    };
+
+    return todo.authorizeUser(ctx).then(()=>{
+      expect(ctx.reply.mock.calls[0][0]).toEqual(expect.stringContaining("client_id"));
+    });
+  });
+
+  test("requestRefresh sets ms_todo_token in prefs", ()=>{
+    axios.post.mockResolvedValue({data: {
+      access_token: "43",
+    }});
+
+    return todo.requestRefresh().then(()=>{
+      expect(preferences.set.mock.calls[0][0]).toBe("ms_todo_token");
+    });
+  });
 });
