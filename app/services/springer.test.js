3e1af15f6a70c623a90597a95f4fbfd8ced79064
Erik Littwin <erik.littwin@gmail.com>
2020-04-15 08:34:45 +0200

    better springer test

--- app/services/springer.test.js
+++ app/services/springer.test.js
@@ -16,4 +16,12 @@ describe("Springer service", () => {
       expect(data).toEqual(searchResponse);
     });
   });
+
+  test("rejects if API error occurs", () => {
+    axios.get.mockRejectedValue(new Error());
+
+    return springer.getByKeyword("user experience").catch((error) => {
+      expect(error).toBeDefined();
+    });
+  });
 });
